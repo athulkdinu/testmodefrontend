633b5561c04a0affbdc80f4e23d2a52c95a53203
athulkdinu <athulkdinu@gmail.com>
2025-12-18 09:25:45 +0530

    last

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,4 +1,4 @@
 
-const SERVERURL =  "http://localhost:3000";
-
+// const SERVERURL =  "http://localhost:3000";
+const SERVERURL =  "https://testmodebackend.onrender.com";
 export default SERVERURL;
